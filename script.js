b5abe96786be0f203efe913ecca3999c2b717a36
Bekislam09 <email@example.com>
2024-10-16 16:27:01 +0600

    first commit

--- script.js
+++ script.js
@@ -8,14 +8,24 @@ const formatNum = num => num < 10 && num > -10 ? `0${num}` : num;
 
 
 
-setInterval(()=>{
+const interval = setInterval(()=>{
     let date = new Date()
-    let bekislam = new Date(2024, 9, 16, 0, 0)
+    let bekislam = new Date(2024, 9, 16, 16, 5)
     let difference = bekislam.getTime() - date.getTime()
 
-    if (difference <= 0) {
-        document.body.innerHTML = '<h1 style="text-align: center; margin-top:250px; font-size: 100px; color: white;">С Днём Рождения!</h1>';
-        return;
+    if (difference <= 0){
+        clearInterval(interval)
+        days.textContent = '00'
+        hours.textContent = '00'
+        minutes.textContent = '00'
+        seconds.textContent = '00'
+
+        const birthday = document.createElement('h1');
+        birthday.textContent = 'С Днём Рождения!';
+        document.body.appendChild(birthday);
+        
+        return
+        
     }
 
     days.textContent = formatNum(Math.floor(difference  / 1000 / 60 / 60 / 24))
